41bf9b649b392ca0ed186c841acee57443d5e227
CiTuX <info@citux.ch>
2013-01-27 02:11:01 +0100

    Gradle, submodules

--- src/ch/citux/twitchdroid/HomeActivity.java
+++ src/ch/citux/twitchdroid/HomeActivity.java
@@ -2,10 +2,10 @@ package ch.citux.twitchdroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
 
-public class HomeActivity extends Activity
+public class HomeActivity extends SherlockActivity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
