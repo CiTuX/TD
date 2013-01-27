d095c9678704846173b00dfd11554c161649fa6f
CiTuX <info@citux.ch>
2013-01-27 17:49:24 +0100

    Added DataDroid

---
+++ src/ch/citux/twitchdroid/data/service/SkeletonService.java
@@ -0,0 +1,34 @@
+/**
+ * 2011 Foxykeep (http://datadroid.foxykeep.com)
+ * <p>
+ * Licensed under the Beerware License : <br />
+ * As long as you retain this notice you can do whatever you want with this stuff. If we meet some
+ * day, and you think this stuff is worth it, you can buy me a beer in return
+ */
+
+package ch.citux.twitchdroid.data.service;
+
+import android.content.Intent;
+
+import com.foxykeep.datadroid.service.RequestService;
+
+/**
+ * This class is called by the {@link SkeletonRequestManager} through the {@link android.content.Intent} system.
+ *
+ * @author Foxykeep
+ */
+public final class SkeletonService extends RequestService {
+
+    // TODO by default only one concurrent worker thread will be used. If you want to change that,
+    // override the getMaximumNumberOfThreads() method
+
+    @Override
+    public Operation getOperationForType(int requestType) {
+        switch (requestType) {
+            // TODO : Add a case per worker where you do the following things :
+            // - create the corresponding Operation and return it
+            // See the PoC if you need more information.
+        }
+        return null;
+    }
+}
--- src/ch/citux/twitchdroid/ui/HomeActivity.java
+++ src/ch/citux/twitchdroid/ui/HomeActivity.java
@@ -1,7 +1,8 @@
-package ch.citux.twitchdroid;
+package ch.citux.twitchdroid.ui;
 
 import android.app.Activity;
 import android.os.Bundle;
+import ch.citux.twitchdroid.R;
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class HomeActivity extends SherlockActivity
