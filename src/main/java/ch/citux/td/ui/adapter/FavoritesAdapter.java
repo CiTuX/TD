dc91f2d3187279dd93f104826b434df447507bb6
CiTuX <info@citux.ch>
2014-02-06 22:56:24 +0100

    default gradle structure use of picasso

---
+++ src/main/java/ch/citux/td/ui/adapter/FavoritesAdapter.java
@@ -0,0 +1,170 @@
+package ch.citux.td.ui.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+import ch.citux.td.R;
+import ch.citux.td.data.model.Channel;
+import ch.citux.td.data.model.Logo;
+import ch.citux.td.data.model.Status;
+
+public class FavoritesAdapter extends BaseAdapter {
+
+    private static final int ANIMATION_DURATION = 300;
+
+    private ArrayList<Channel> data;
+    private LayoutInflater inflater;
+    private Picasso picasso;
+    private Animation fadeOut;
+    private Animation fadeIn;
+
+    public FavoritesAdapter(Context context) {
+        init(context, new ArrayList<Channel>());
+    }
+
+    public FavoritesAdapter(Context context, ArrayList<Channel> data) {
+        init(context, data);
+    }
+
+    private void init(Context context, ArrayList<Channel> data) {
+        this.data = data;
+        this.inflater = LayoutInflater.from(context);
+        this.picasso = Picasso.with(context);
+    }
+
+    private void initAnimation() {
+        fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setDuration(ANIMATION_DURATION);
+
+        fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator());
+        fadeIn.setDuration(ANIMATION_DURATION);
+        fadeIn.setStartOffset(ANIMATION_DURATION);
+        fadeIn.setFillBefore(false);
+        fadeIn.setFillAfter(true);
+    }
+
+    public void setData(ArrayList<Channel> data) {
+        this.data = data;
+        notifyDataSetChanged();
+    }
+
+    public ArrayList<Channel> getData() {
+        return data;
+    }
+
+    public void clear() {
+        data.clear();
+        notifyDataSetInvalidated();
+    }
+
+    public void updateChannel(Channel channel) {
+        for (int i = 0; i != data.size(); i++) {
+            Channel item = data.get(i);
+            if (item.getName().equals(channel.getName())) {
+                item.setStatus(channel.getStatus());
+                data.set(i, item);
+            }
+        }
+//        updateStatus(channel.getStatus(), holders.get(channel.getName()));
+    }
+
+    private void updateStatus(Status status, ViewHolder holder) {
+        if (status == null || holder == null) {
+            return;
+        }
+
+        //UNKNOWN
+        int color = R.color.status_unknown;
+        boolean visibilityLbl = false;
+        boolean visibilityPrg = true;
+        int text = R.string.channel_offline;
+        initAnimation();
+
+        switch (status) {
+            case ONLINE:
+                text = R.string.channel_online;
+                visibilityLbl = true;
+                visibilityPrg = false;
+                color = R.color.status_online;
+                break;
+            case OFFLINE:
+                text = R.string.channel_offline;
+                visibilityLbl = true;
+                visibilityPrg = false;
+                color = R.color.status_offline;
+                break;
+        }
+        holder.lblStatus.setText(text);
+        holder.lblStatus.setVisibility(visibilityLbl ? View.VISIBLE : View.INVISIBLE);
+        holder.lblStatus.setAnimation(visibilityLbl ? fadeIn : fadeOut);
+        holder.prgStatus.setVisibility(visibilityPrg ? View.VISIBLE : View.INVISIBLE);
+        holder.statusIndicator.setBackgroundResource(color);
+
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Channel getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return getItem(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Channel channel = getItem(position);
+        ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.list_item_favorites, null);
+            if (convertView != null) {
+                holder.imgLogo = (ImageView) convertView.findViewById(R.id.imgLogo);
+                holder.lblTitle = (TextView) convertView.findViewById(R.id.lblTitle);
+                holder.lblStatus = (TextView) convertView.findViewById(R.id.lblStatus);
+                holder.prgStatus = (ProgressBar) convertView.findViewById(R.id.prgStatus);
+                holder.statusIndicator = convertView.findViewById(R.id.statusIndicator);
+                convertView.setTag(holder);
+            }
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        picasso.load(channel.getLogo(Logo.SMALL)).placeholder(R.drawable.default_channel_logo_small).into(holder.imgLogo);
+        holder.lblTitle.setText(channel.getTitle());
+        updateStatus(channel.getStatus(), holder);
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+        ImageView imgLogo;
+        TextView lblTitle;
+        TextView lblStatus;
+        ProgressBar prgStatus;
+        View statusIndicator;
+    }
+
+}
---
+++ src/main/java/ch/citux/td/ui/fragments/ChannelFragment.java
@@ -0,0 +1,173 @@
+package ch.citux.td.ui.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.holoeverywhere.LayoutInflater;
+
+import ch.citux.td.R;
+import ch.citux.td.config.TDConfig;
+import ch.citux.td.data.model.Channel;
+import ch.citux.td.data.model.Logo;
+import ch.citux.td.data.model.Status;
+import ch.citux.td.data.model.StreamPlayList;
+import ch.citux.td.data.model.Video;
+import ch.citux.td.data.model.Videos;
+import ch.citux.td.data.worker.TDBasicCallback;
+import ch.citux.td.data.worker.TDTaskManager;
+import ch.citux.td.ui.adapter.ArchiveAdapter;
+import ch.citux.td.ui.dialogs.ErrorDialogFragment;
+import ch.citux.td.ui.widget.EmptyView;
+import ch.citux.td.util.Log;
+
+public class ChannelFragment extends TDFragment<Videos> implements View.OnClickListener, AdapterView.OnItemClickListener {
+
+    public static final String CHANNEL = "channel";
+
+    private ArchiveAdapter adapter;
+    private EmptyView empty;
+    private ViewGroup content;
+    private ImageView imgLogo;
+    private TextView lblTitle;
+    private TextView lblStatus;
+    private Button btnStream;
+    private Channel channel;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View contentView = inflater.inflate(R.layout.channel_detail, null, false);
+        if (contentView != null) {
+            empty = (EmptyView) contentView.findViewById(R.id.empty);
+            content = (ViewGroup) contentView.findViewById(R.id.content);
+            imgLogo = (ImageView) contentView.findViewById(R.id.imgLogo);
+            lblTitle = (TextView) contentView.findViewById(R.id.lblTitle);
+            lblStatus = (TextView) contentView.findViewById(R.id.lblStatus);
+            btnStream = (Button) contentView.findViewById(R.id.btnStream);
+        }
+        return contentView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getArguments() != null && getArguments().containsKey(CHANNEL)) {
+            updateChannel((Channel) getArguments().get(CHANNEL));
+        } else {
+            empty.setText(R.string.channel_detail_empty);
+        }
+        btnStream.setOnClickListener(this);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshData();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (empty != null) {
+            empty.showProgress();
+        }
+    }
+
+    @Override
+    public void loadData() {
+        if (adapter != null) {
+            adapter.clear();
+        }
+        if (channel != null) {
+            TDTaskManager.getArchives(this, channel.getName());
+        }
+    }
+
+    public void updateChannel(final Channel channel) {
+        this.channel = channel;
+        Picasso.with(getActivity()).load(channel.getLogo(Logo.LARGE)).placeholder(R.drawable.default_channel_logo_medium).into(imgLogo);
+        lblTitle.setText(channel.getTitle());
+        lblStatus.setText(channel.getStatus().getText());
+        btnStream.setVisibility(channel.getStatus() == Status.ONLINE ? View.VISIBLE : View.GONE);
+
+        empty.showProgress();
+        empty.setVisibility(View.GONE);
+        content.setVisibility(View.VISIBLE);
+
+        getListView().setEmptyView(empty);
+        loadData();
+    }
+
+    private void playVideo(String title, String url) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.parse(url), TDConfig.MIME_FLV);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TDTaskManager.getStreamPlaylist(new StreamPlaylistCallback(this), channel.getName());
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Video video = adapter.getItem(position);
+        TDTaskManager.getVideo(new GetVideoCallback(this), video.getId());
+    }
+
+    @Override
+    public void onResponse(Videos response) {
+        empty.setText(R.string.channel_archives_empty);
+        if (adapter == null) {
+            adapter = new ArchiveAdapter(getActivity(), response.getVideos());
+            setListAdapter(adapter);
+        } else {
+            adapter.setData(response.getVideos());
+        }
+    }
+
+    private class GetVideoCallback extends TDBasicCallback<Video> {
+
+        protected GetVideoCallback(Object caller) {
+            super(caller);
+        }
+
+        @Override
+        public void onResponse(Video response) {
+            if (response.getUrl() != null) {
+                playVideo(response.getTitle(), response.getUrl());
+            }
+        }
+    }
+
+    private class StreamPlaylistCallback extends TDBasicCallback<StreamPlayList> {
+
+        protected StreamPlaylistCallback(Object caller) {
+            super(caller);
+        }
+
+        @Override
+        public void onResponse(StreamPlayList response) {
+            Log.d(this, "Streams :" + response.getStreams().toString());
+            if (response.getStreams() != null && response.getStreams().size() > 0) {
+                //TODO quality from settings
+                String url = response.getStream(StreamPlayList.QUALITY_MEDIUM);
+                if (url != null) {
+                    playVideo(channel.getTitle(), url);
+                }
+            } else {
+                ErrorDialogFragment.ErrorDialogFragmentBuilder builder = new ErrorDialogFragment.ErrorDialogFragmentBuilder(getActivity());
+                builder.setMessage("Stream offline :(").setTitle("Error").show();
+            }
+        }
+    }
+}
